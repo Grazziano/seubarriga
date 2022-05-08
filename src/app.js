31ca72705a1c1832e6a1a458e6142a5df6e43722
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-05-07 23:11:44 -0300

    Deve inserir usuário com sucesso

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 const app = require('express')();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send();
@@ -10,6 +13,11 @@ app.get('/users', (req, res) => {
     { name: 'Lara Croft', email: 'lara@mail.com' },
   ];
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  res.status(201).json({ name, email });
 });
 
 module.exports = app;
--- test/user.test.js
+++ test/user.test.js
@@ -11,3 +11,13 @@ test('Deve listar todos os usuários', () => {
       expect(res.body[0]).toHaveProperty('name', 'Steve Rogers');
     });
 });
+
+test('Deve inserir usuário com sucesso', () => {
+  return request(app)
+    .post('/users')
+    .send({ name: 'Clark Kent', email: 'ck@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Clark Kent');
+    });
+});
