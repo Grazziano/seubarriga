63bd85e1f104f83b59be6731a48124293938b738
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-05-07 22:56:54 -0300

    Deve listar os usuários

--- src/app.js
+++ src/app.js
@@ -4,4 +4,12 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'Steve Rogers', email: 'steve@mail.com' },
+    { name: 'Lara Croft', email: 'lara@mail.com' },
+  ];
+  res.status(200).json(users);
+});
+
 module.exports = app;
